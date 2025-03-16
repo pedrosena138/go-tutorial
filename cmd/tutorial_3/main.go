5759b8dc9780fcf6a0cde1e7b9f763d179e4915d
Pedro Sena <pedrolima121+github@gmail.com>
2025-03-15 22:30:01 -0300

    functions

---
+++ cmd/tutorial_3/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func greetings(name string) {
+	fmt.Println("Greetings", name)
+}
+
+func intDivision(numerator, denominator int) (int, int, error) {
+	var err error
+	if denominator == 0 {
+		err = errors.New("error: division by zero")
+		return 0, 0, err
+	}
+
+	var result int = numerator / denominator
+	var remainder int = numerator % denominator
+	return result, remainder, err
+}
+func main() {
+	greetings("John")
+
+	const num int = 11
+	const den int = 0
+	var result, remainder, err = intDivision(num, den)
+	switch {
+	case err != nil:
+		fmt.Println(err.Error())
+	case remainder == 0:
+		fmt.Printf("The result is: %v", result)
+	default:
+		fmt.Printf("Division: %v. Remainder: %v", result, remainder)
+	}
+
+	switch remainder {
+	case 0:
+		fmt.Println("Exact division")
+	case 1, 2:
+		fmt.Println("Division was close")
+	default:
+		fmt.Println("Division was not close")
+	}
+
+}
